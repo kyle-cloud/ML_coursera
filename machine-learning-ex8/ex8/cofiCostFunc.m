011b1959e8d86710408f2c6498cd7423a2d15168
kyle-cloud <1139467635@qq.com>
2020-01-29 20:58:23 +0800

    gradient

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,6 +40,8 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 J = 1 / 2 * sum(sum(((X * Theta' - Y) .* R) .^2));
+X_grad = (X * Theta' - Y) .* R * Theta;
+Theta_grad = ((X * Theta' - Y) .* R)' * X;
 
 
 
